5daf5e7e15f7a68e41f4253bad0f73e17b329bae
@pallavi-gope <pallavigope6@gmail.com>
2022-03-09 10:27:24 +0530

    Updates

---
+++ precache-manifest.5848606b4baa498a2f8a3d8665d88171.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ecd0f969100d48fd9232",
+    "url": "/TextUtils/static/css/main.a42d0ed4.chunk.css"
+  },
+  {
+    "revision": "ecd0f969100d48fd9232",
+    "url": "/TextUtils/static/js/main.ecd0f969.chunk.js"
+  },
+  {
+    "revision": "0d630a3e1f21db7817dc",
+    "url": "/TextUtils/static/js/1.0d630a3e.chunk.js"
+  },
+  {
+    "revision": "f1dc41ae5e58741994f8",
+    "url": "/TextUtils/static/js/2.f1dc41ae.chunk.js"
+  },
+  {
+    "revision": "86c1c36d87b5c85ec6da",
+    "url": "/TextUtils/static/js/runtime~main.86c1c36d.js"
+  },
+  {
+    "revision": "25cab80f7147a515bb688da72603953a",
+    "url": "/TextUtils/index.html"
+  }
+];
